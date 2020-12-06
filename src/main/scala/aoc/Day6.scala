068d27085c40e36bdd3bf84a8b571c026b3b140c
Mikko Matilainen <mikkom@iki.fi>
2020-12-06 09:32:14 +0200

    Solution for day 6

---
+++ src/main/scala/aoc/Day6.scala
@@ -0,0 +1,37 @@
+package aoc
+
+import scala.io.Source
+import cats.syntax.all._
+import cats.effect.{IO, IOApp}
+
+def answer1(input: List[String]) =
+  val (total, _) =
+    input.foldLeft((0, Set.empty[Char])) { case ((total, groupSet), form) =>
+      if form.isEmpty() then
+        (total + groupSet.size, Set.empty)
+      else
+        (total, groupSet ++ form)
+    }
+  total
+
+def answer2(input: List[String]) =
+  val (total, _) =
+    input.foldLeft((0, none[Set[Char]])) {
+      case ((total, groupSet), "") =>
+        (total + groupSet.map(_.size).getOrElse(0), None)
+      case ((total, None), form) =>
+        (total, Some(form.toSet))
+      case ((total, Some(groupSet)), form) =>
+        (total, Some(groupSet.intersect(form.toSet)))
+    }
+  total
+
+object Day6 extends IOApp.Simple:
+  override def run: IO[Unit] =
+    for
+      input <- IO(
+        Source.fromResource("input-day6.txt").getLines().concat(List("")).toList
+      )
+      _ <- IO(println(answer1(input)))
+      _ <- IO(println(answer2(input)))
+    yield ()
